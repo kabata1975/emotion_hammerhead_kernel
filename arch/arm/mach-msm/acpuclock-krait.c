77a17e23e1bb2154e34efad34e6d0cf634da604d
Paul Reioux <reioux@gmail.com>
2013-10-20 22:30:36 -0500

    Voltage Control: initial voltage control for MSM Snapdragon 800 SOC
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: flar2 <asegaert@gmail.com>
    
    Conflicts:
    	arch/arm/mach-msm/acpuclock-krait.c

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -937,6 +937,7 @@ static void __init bus_init(const struct l2_level *l2_level)
 		dev_err(drv.dev, "initial bandwidth req failed (%d)\n", ret);
 }
 
+<<<<<<< HEAD
 #define MAX_VDD 1300
 #define MIN_VDD 600
 
@@ -953,10 +954,32 @@ ssize_t acpuclk_get_vdd_levels_str(char *buf)
                            drv.acpu_freq_tbl[i].vdd_core/1000 );
             }
 		}
+=======
+#ifdef CONFIG_CPU_VOLTAGE_TABLE
+
+#define HFPLL_MIN_VDD		 800000
+#define HFPLL_MAX_VDD		1350000
+
+ssize_t acpuclk_get_vdd_levels_str(char *buf) {
+
+	int i, len = 0;
+
+	if (buf) {
+		mutex_lock(&driver_lock);
+
+		for (i = 0; drv.acpu_freq_tbl[i].speed.khz; i++) {
+			/* updated to use uv required by 8x60 architecture - faux123 */
+			len += sprintf(buf + len, "%8lu: %8d\n", drv.acpu_freq_tbl[i].speed.khz,
+				drv.acpu_freq_tbl[i].vdd_core );
+		}
+
+		mutex_unlock(&driver_lock);
+>>>>>>> b194f25... Voltage Control: initial voltage control for MSM Snapdragon 800 SOC
 	}
 	return len;
 }
 
+<<<<<<< HEAD
 ssize_t acpuclk_set_vdd(char *buf)
 {
 	unsigned int cur_volt;
@@ -988,6 +1011,32 @@ ssize_t acpuclk_set_vdd(char *buf)
 	}
 	return ret;
 }
+=======
+/* updated to use uv required by 8x60 architecture - faux123 */
+void acpuclk_set_vdd(unsigned int khz, int vdd_uv) {
+
+	int i;
+	unsigned int new_vdd_uv;
+
+	mutex_lock(&driver_lock);
+
+	for (i = 0; drv.acpu_freq_tbl[i].speed.khz; i++) {
+		if (khz == 0)
+			new_vdd_uv = min(max((unsigned int)(drv.acpu_freq_tbl[i].vdd_core + vdd_uv),
+				(unsigned int)HFPLL_MIN_VDD), (unsigned int)HFPLL_MAX_VDD);
+		else if ( drv.acpu_freq_tbl[i].speed.khz == khz)
+			new_vdd_uv = min(max((unsigned int)vdd_uv,
+				(unsigned int)HFPLL_MIN_VDD), (unsigned int)HFPLL_MAX_VDD);
+		else 
+			continue;
+
+		drv.acpu_freq_tbl[i].vdd_core = new_vdd_uv;
+	}
+	pr_warn("faux123: user voltage table modified!\n");
+	mutex_unlock(&driver_lock);
+}
+#endif	/* CONFIG_CPU_VOTALGE_TABLE */
+>>>>>>> b194f25... Voltage Control: initial voltage control for MSM Snapdragon 800 SOC
 
 #ifdef CONFIG_CPU_FREQ_MSM
 static struct cpufreq_frequency_table freq_table[NR_CPUS][35];
